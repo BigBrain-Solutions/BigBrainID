1c851aefd2c4328c00c38b3c809cf0c0abc77654
Lukeuke <lukasz.mrowiec10102004@gmail.com>
2022-11-07 23:25:44 +0100

    Initial setup for Clean Architecture. Docker compose

---
+++ Domain/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string PasswordHash { get; set; } = default!;
+    public string Salt { get; set; } = default!;
+}
---
+++ WebAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new User());
+    }
+}
